41179035ca0363637177024d338975e53483cdb6
ImmortanRaptus <49250280+ImmortanRaptus@users.noreply.github.com>
2019-07-31 00:43:14 -0400

    Massive Rework

--- routes/AccountAPI/AdminAPI.js
+++ routes/AccountAPI/AdminAPI.js
@@ -11,6 +11,7 @@ var config     = require('../Config');
 var mysql      = require('mysql');           // Used for MySQL queries
 var bodyParser = require('body-parser');     // Used for getting request values
 var bcrypt     = require('bcrypt');          // Used for encrypting passwords
+var request    = require('request');         // Used to workaround stuff
 
 var app = express();
 
@@ -41,19 +42,31 @@ app.use(bodyParser.urlencoded({extended : true})); // Supports encoded bodies
 // ** ROUTE **
 // -----------------------------------------------------------------------------
 
-router.post('/CreateAccount', function(req, res, next) {
+router.post('/CreateAccount', createAccount);
+router.post('/CreateAccount/emailCheck', emailCheck);
+router.post('/CreateAccount/insertPerson', insertPerson);
+router.post('/CreateAccount/Exists', exists);
+
+function exists(req, res, next) {
+    console.log("User Account Exists");
+    res.send("User Account Exists");
+}
+
+function insertPerson(req, res, next) {
   var pass = req.body.password
   var email = req.body.email
   var VID = req.body.VID
   var fname= req.body.fname
   var lname= req.body.lname
-  if(!req.body.email || !req.body.password || !req.body.VID || !req.body.fname || !req.body.lname)
-    res.send("CreateAccount Missing Parameters");
-  else
-    res.redirect(307, '/API/Accounts/Admin/CreateAccount/emailCheck');
-});
+  bcrypt.hash(req.body.password, config.hashing.saltRounds, function(err, hash) {
+	if(err) throw error;
+  connection.query('INSERT INTO Admin (Venue_VID, email, firstName, lastName) VALUES (?,?,?,?); INSERT INTO Admin_Password (Admin_AID, password) VALUES (LAST_INSERT_ID(),?);', [VID, email, fname, lname, hash] , function (error, results, fields) {if (error) throw error;});
+  console.log("Person Successfully Inserted");
+  res.end();
+  });
+}
 
-router.post('/CreateAccount/emailCheck', function(req, res, next) {
+function emailCheck(req, res, next) {
   var pass = req.body.password
   var email = req.body.email
   var VID = req.body.VID
@@ -62,30 +75,27 @@ router.post('/CreateAccount/emailCheck', function(req, res, next) {
   connection.query('SELECT count(1) AS emailCheck FROM Admin WHERE email = ?', [req.body.email] , function (error, results, fields) {
       if (error) throw error;
       if (results[0].emailCheck != 0){
-         //res.redirect(307, '/API/Accounts/Admin/CreateAccount/passCheck');
-         res.redirect(307, '/API/Accounts/Admin/CreateAccount/exists');
+         exists(req, res, next);
       }
       else
-        res.redirect(307, '/API/Accounts/Admin/CreateAccount/insertPerson');
+        insertPerson(req, res, next);
   });
-});
+}
 
-router.post('/CreateAccount/insertPerson', function(req, res, next) {
+
+function createAccount(req, res, next) {
   var pass = req.body.password
   var email = req.body.email
   var VID = req.body.VID
   var fname= req.body.fname
   var lname= req.body.lname
-  bcrypt.hash(req.body.password, config.hashing.saltRounds, function(err, hash) {
-	if(err) throw error;
-    connection.query('INSERT INTO Admin (Venue_VID, email, firstName, lastName) VALUES (?,?,?,?); INSERT INTO Admin_Password (Admin_AID, password) VALUES (LAST_INSERT_ID(),?);', [VID, email, fname, lname, hash] , function (error, results, fields) {if (error) throw error;});
-    res.redirect('/');
-  });
-});
-
-router.post('/CreateAccount/Exists', function(req, res, next) {
-    res.send("User Account Exists");
-});
+  if(!req.body.email || !req.body.password || !req.body.VID || !req.body.fname || !req.body.lname){
+    console.log("CreateAccount Missing Parameters")
+    res.send("CreateAccount Missing Parameters");
+  }
+  else
+    emailCheck(req, res, next);
+}
 
 router.post('/Login', function(req, res, next) {
     var email = req.body.email;

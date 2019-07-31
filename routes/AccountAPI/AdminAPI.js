beea674c612f3346804a6ee4add2bccb7722530f
ImmortanRaptus <49250280+ImmortanRaptus@users.noreply.github.com>
2019-07-31 13:08:22 -0400

    Update AdminAPI.js

--- routes/AccountAPI/AdminAPI.js
+++ routes/AccountAPI/AdminAPI.js
@@ -61,8 +61,8 @@ function insertPerson(req, res, next) {
   bcrypt.hash(req.body.password, config.hashing.saltRounds, function(err, hash) {
 	if(err) throw error;
   connection.query('INSERT INTO Admin (Venue_VID, email, firstName, lastName) VALUES (?,?,?,?); INSERT INTO Admin_Password (Admin_AID, password) VALUES (LAST_INSERT_ID(),?);', [VID, email, fname, lname, hash] , function (error, results, fields) {if (error) throw error;});
+  console.log("Person Successfully Inserted");
   res.json({"err":"0"});
-  res.end();
   });
 }
 

1386ab00bce05d4f3b1562c5bd129a87b25edde0
ImmortanRaptus <49250280+ImmortanRaptus@users.noreply.github.com>
2019-07-31 21:32:48 -0400

    big update

--- routes/StallAPI.js
+++ routes/StallAPI.js
@@ -38,6 +38,7 @@ app.use(bodyParser.urlencoded({extended : true})); // Supports encoded bodies
 router.post("/insert", insert);
 router.post("/delete", deleteStall);
 router.post("/edit", edit); 
+router.post("/vacate", vacate);
 
 function insertCont(req, res, next) {
   var sql = `INSERT INTO Stall (stallName, Venue_VID ) VALUES (?, ?)`;
@@ -97,5 +98,33 @@ function deleteStall(req, res, next) {
   else deleteCont(req,res,next);
 }
 
+function vacate(req, res, next) {
+  // get data from forms and add to the table called user..
+  if (!req.body.stallName) res.json({"err": "1", "text":"Missing Parameters"});
+  else vacateCont(req,res,next);
+}
+
+function vacateCont(req, res, next) {
+  var sql = "SELECT R.RID FROM Request R, Stall S WHERE R.Client_CID = S.Client_CID AND S.SID = ?;";
+  var sql2 = "UPDATE Request SET status = 0 WHERE RID = ?";
+  var sql3 = "UPDATE Stall SET Client_CID = NULL WHERE SID = ?";
+  connection.query(sql, [req.body.SID], function(err, data) {
+    if(err) throw err;
+    else {
+      let rid = data[0].RID
+      connection.query(sql2, [rid], function(err, data) {
+        if(err) throw err;
+        else {
+          connection.query(sql3, [req.body.SID], function(err, data) {
+            if(err) throw err;
+            else {
+              res.json({"err":"0", "text": "Successful Removal of Client"})
+            }
+          });
+        }
+      });
+    }
+  });
+}
 
 module.exports = router;
